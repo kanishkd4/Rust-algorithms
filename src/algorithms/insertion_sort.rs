a435d068f77ad14b743d62ba7af113c4333b02db
kanishkd4 <kanishkd4@gmail.com>
2018-08-30 16:24:44 +0530

    working on insertion sort

--- src/algorithms/insertion_sort.rs
+++ src/algorithms/insertion_sort.rs
@@ -1,3 +1,36 @@
+use std::vec::Vec;
+
 pub fn test_print() {
     println!("Called algorithms.insertion_sort");
+}
+
+pub fn test_string_comparison() {
+    if "Bell" < "Mark" {
+        println!("Working");
+    }
+    else {
+        println!("Not working");
+    }
+}
+
+pub fn insertion_sort(vec: Vec<&str>) {
+    println!("Original vector: {:?}", vec);
+    let mut new_vector: Vec<&str> = Vec::new();
+    for (i, item) in vec.into_iter().enumerate() {
+        println!("i: {}", i);
+        if i == 0 {
+            new_vector.push(item);
+        }
+        else {
+            for j in 0..new_vector.len() {
+                println!("j: {}", j);
+                if item < new_vector[j] {
+                    // split the vector into at j and add item to the first one
+                    // combine the vectors to new_vector again
+                }
+            }
+        }
+    }
+    
+    println!("new_vector: {:?}", new_vector);
 }
--- src/bin/test.rs
+++ src/bin/test.rs
@@ -3,4 +3,7 @@ use algorithms::insertion_sort;
 
 fn main() {
     insertion_sort::test_print();
+    insertion_sort::test_string_comparison();
+    let vec: Vec<&str> = vec!["10", "2", "3"];
+    insertion_sort::insertion_sort(vec);
 }
