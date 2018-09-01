9f525d2ab9a5f1f1f04b9d91f6d10b0b53c5fe40
kanishkd4 <kanishkd4@gmail.com>
2018-09-01 13:02:12 +0530

    scope error; need to check on different installation

--- src/algorithms/insertion_sort.rs
+++ src/algorithms/insertion_sort.rs
@@ -13,18 +13,18 @@ pub fn test_string_comparison() {
     }
 }
 
-pub fn insertion_sort(vec: Vec<&str>) {
+pub fn insertion_sort(vec: Vec<i32>) {
     println!("Original vector: {:?}", vec);
-    let mut new_vector: Vec<&str> = Vec::new();
-    for (i, item) in vec.into_iter().enumerate() {
+    let mut new_vector: Vec<i32> = Vec::new();
+    for (i, item) in vec.iter().enumerate() {
         println!("i: {}", i);
         if i == 0 {
-            new_vector.push(item);
+            new_vector.push(*item);
         }
         else {
             for j in 0..new_vector.len() {
                 println!("j: {}", j);
-                if item < new_vector[j] {
+                if item < &new_vector[j] {
                     // split the vector into at j and add item to the first one
                     // combine the vectors to new_vector again
                 }
@@ -33,4 +33,8 @@ pub fn insertion_sort(vec: Vec<&str>) {
     }
     
     println!("new_vector: {:?}", new_vector);
+    let mut new_vec_test = &vec[..2];
+    println!("new vec test: {:?}", new_vec_test);
+    let vec = new_vec_test.extend_from_slice(&mut new_vector);
+    println!("pushing one vector into another: {:?}", vec);
 }
--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,9 +1,19 @@
 extern crate algorithms;
 use algorithms::insertion_sort;
+use std::vec::Vec;
 
 fn main() {
-    insertion_sort::test_print();
-    insertion_sort::test_string_comparison();
-    let vec: Vec<&str> = vec!["10", "2", "3"];
-    insertion_sort::insertion_sort(vec);
+    // insertion_sort::test_print();
+    // insertion_sort::test_string_comparison();
+    // let vec: Vec<i32> = vec![10, 2, 3]; // std::vec::Vec
+    // insertion_sort::insertion_sort(vec);
+    // let mut vec = vec![1];
+    // vec.extend_from_slice(&[2, 3, 4]);
+    // assert_eq!(vec, [1, 2, 3, 4]);
+
+    let mut vec = vec![1];
+    let vec1 = [2, 3, 4];
+    vec.extend_from_slice(&vec1);
+    assert_eq!(vec, [1, 2, 3, 4]);
+    println!("vec: {:?}", vec);
 }
