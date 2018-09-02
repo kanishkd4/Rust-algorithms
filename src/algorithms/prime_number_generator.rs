4e6fa8d003d7ed56d2119c2663169df6527d65d2
kanishkd4 <kanishkd4@gmail.com>
2018-09-02 15:52:30 +0530

    sieve of eratosthenes implemented using a vector

--- src/algorithms/prime_number_generator.rs
+++ src/algorithms/prime_number_generator.rs
@@ -1,3 +1,27 @@
-// pub fn sieve_of_eratosthenes(n: usize) -> Vec<u32> {
+pub fn sieve_of_eratosthenes(n: usize) -> Vec<u32> {
+    let n_total: usize = n;
+    let mut n_vec = vec![1; n_total];
+    n_vec[0] = 0;
+    n_vec[1] = 0;
+    let n = n_total as f32;
+    let n_root = n.sqrt().round() as usize;
+    let n = n as usize;
 
-// }
+    for i in 2..=n_root {
+        if n_vec[i] == 1 {
+            let j_iter = i.pow(2)..n;
+            for j in j_iter.into_iter().step_by(i) {
+                n_vec[j] = 0;
+            }
+        }
+    }
+    let mut primes = Vec::new();
+    let upper_limit = n as u32;
+    for i in 0..upper_limit {
+        if n_vec[i as usize] == 1 {
+            primes.push(i);
+        }
+    }
+    println!("Primes up to {}: {:?}", n, primes.len());
+    return primes;
+}
--- src/bin/prime.rs
+++ src/bin/prime.rs
@@ -3,30 +3,29 @@ use algorithms::prime_number_generator;
 
 
 fn main() {
-    const N: usize = 1000_;
-    let mut n_array: [u64; N] = [1; N];
-    n_array[0] = 0;
-    n_array[1] = 0;
-    let n = N as f32;
-    let n_root = n.sqrt().round() as usize;
-    let n = n as usize;
+    // let n_total: usize = 1000_000_000_000;
+    // let mut n_vec = vec![1; n_total];
+    // n_vec[0] = 0;
+    // n_vec[1] = 0;
+    // let n = n_total as f32;
+    // let n_root = n.sqrt().round() as usize;
+    // let n = n as usize;
 
-    for i in 2..=n_root {
-        if n_array[i] == 1 {
-            let j_iter = i.pow(2)..n;
-            for j in j_iter.into_iter().step_by(i) {
-                n_array[j] = 0;
-            }
-        }
-    }
-    let mut primes = Vec::new();
-    let upper_limit = n as u32;
-    for i in 0..upper_limit {
-        if n_array[i as usize] == 1 {
-            primes.push(i);
-        }
-    }
-    println!("Primes up to {}: {:?}", n, primes.len());
-
-
+    // for i in 2..=n_root {
+    //     if n_vec[i] == 1 {
+    //         let j_iter = i.pow(2)..n;
+    //         for j in j_iter.into_iter().step_by(i) {
+    //             n_vec[j] = 0;
+    //         }
+    //     }
+    // }
+    // let mut primes = Vec::new();
+    // let upper_limit = n as u32;
+    // for i in 0..upper_limit {
+    //     if n_vec[i as usize] == 1 {
+    //         primes.push(i);
+    //     }
+    // }
+    // println!("Primes up to {}: {:?}", n, primes.len());
+    prime_number_generator::sieve_of_eratosthenes(1000_000_000);
 }
