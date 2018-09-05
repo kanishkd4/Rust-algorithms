65a739f98d914c8155425b9a96640b47a2db21f1
kanishkd4 <kanishkd4@gmail.com>
2018-09-05 12:50:02 +0530

    insertion sort working with swap; not with slicing and appending

--- src/algorithms/insertion_sort.rs
+++ src/algorithms/insertion_sort.rs
@@ -13,28 +13,49 @@ pub fn test_string_comparison() {
     }
 }
 
-pub fn insertion_sort(vec: Vec<i32>) {
+// was attempting to subset the vector but cannot figure out applying extend_from_slice to &[i32]
+// pub fn insertion_sort_int(vec: Vec<i32>) {
+//     println!("Original vector: {:?}", vec);
+//     let mut new_vector: Vec<i32> = Vec::new();
+//     for (i, item) in vec.iter().enumerate() {
+//         println!("i: {}", i);
+//         if i == 0 {
+//             new_vector.push(*item);
+//         }
+//         else {
+//             for j in 0..new_vector.len() {
+//                 println!("j: {}", j);
+//                 if item < &new_vector[j] {
+//                     // split the vector into at j and add item to the first one
+//                     // combine the vectors to new_vector again
+//                     let mut new_vec1 = &new_vector[0..j];
+//                     let new_vec2 = &new_vector[j..];
+//                     println!("new vec 1: {:?}", new_vec1);
+//                     println!("new vec 2: {:?}", new_vec2);
+//                     let mut new_vector = new_vec1.extend_from_slice(new_vec2);
+//                 }
+//             }
+//         }
+//     }
+    
+//     println!("new_vector: {:?}", new_vector);
+//     let mut new_vec_test = &vec[..2];
+//     println!("new vec test: {:?}", new_vec_test);
+//     // let vec = new_vec_test.extend_from_slice(&mut new_vector);
+//     // println!("pushing one vector into another: {:?}", vec);
+// }
+
+pub fn insertion_sort_int(vec: &mut Vec<i32>) -> &mut Vec<i32> {
     println!("Original vector: {:?}", vec);
-    let mut new_vector: Vec<i32> = Vec::new();
-    for (i, item) in vec.iter().enumerate() {
-        println!("i: {}", i);
-        if i == 0 {
-            new_vector.push(*item);
-        }
-        else {
-            for j in 0..new_vector.len() {
-                println!("j: {}", j);
-                if item < &new_vector[j] {
-                    // split the vector into at j and add item to the first one
-                    // combine the vectors to new_vector again
-                }
+    for i in 0..vec.len() {
+        for j in (0..i).rev() {
+            if vec[j] >= vec[j + 1] {
+                vec.swap(j, j + 1);
+            } else {
+                break
             }
         }
     }
-    
-    println!("new_vector: {:?}", new_vector);
-    let mut new_vec_test = &vec[..2];
-    println!("new vec test: {:?}", new_vec_test);
-    // let vec = new_vec_test.extend_from_slice(&mut new_vector);
-    // println!("pushing one vector into another: {:?}", vec);
+    println!("modified vector: {:?}", vec);
+    return vec;
 }
--- src/bin/test.rs
+++ src/bin/test.rs
@@ -5,15 +5,6 @@ use std::vec::Vec;
 fn main() {
     // insertion_sort::test_print();
     // insertion_sort::test_string_comparison();
-    // let vec: Vec<i32> = vec![10, 2, 3]; // std::vec::Vec
-    // insertion_sort::insertion_sort(vec);
-    // let mut vec = vec![1];
-    // vec.extend_from_slice(&[2, 3, 4]);
-    // assert_eq!(vec, [1, 2, 3, 4]);
-
-    let mut vec = vec![1];
-    let vec1 = [2, 3, 4];
-    vec.extend_from_slice(&vec1);
-    assert_eq!(vec, [1, 2, 3, 4]);
-    println!("vec: {:?}", vec);
+    let mut vec = vec![10, 2, 3]; // std::vec::Vec
+    insertion_sort::insertion_sort_int(&mut vec);
 }
